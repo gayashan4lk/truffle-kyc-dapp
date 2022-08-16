7ac30d71cd7b5b81841a8e4efd20735c1d79937e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-08-16 13:29:45 +0530

    remove unnecessary components

--- client/src/components/Demo/index.jsx
+++ client/src/components/Demo/index.jsx
@@ -1,37 +1,35 @@
-import { useState } from "react";
-import useEth from "../../contexts/EthContext/useEth";
-import Title from "./Title";
-import Cta from "./Cta";
-import Contract from "./Contract";
-import ContractBtns from "./ContractBtns";
-import Desc from "./Desc";
-import NoticeNoArtifact from "./NoticeNoArtifact";
-import NoticeWrongNetwork from "./NoticeWrongNetwork";
+import { useState } from 'react';
+import useEth from '../../contexts/EthContext/useEth';
+import Contract from './Contract';
+import ContractBtns from './ContractBtns';
+import NoticeNoArtifact from './NoticeNoArtifact';
+import NoticeWrongNetwork from './NoticeWrongNetwork';
 
 function Demo() {
-  const { state } = useEth();
-  const [value, setValue] = useState("?");
+	const { state } = useEth();
+	const [value, setValue] = useState('?');
 
-  const demo =
-    <>
-      <Cta />
-      <div className="contract-container">
-        <Contract value={value} />
-        <ContractBtns setValue={setValue} />
-      </div>
-      <Desc />
-    </>;
+	const demo = (
+		<>
+			<div className='contract-container'>
+				<Contract value={value} />
+				<ContractBtns setValue={setValue} />
+			</div>
+		</>
+	);
 
-  return (
-    <div className="demo">
-      <Title />
-      {
-        !state.artifact ? <NoticeNoArtifact /> :
-          !state.contract ? <NoticeWrongNetwork /> :
-            demo
-      }
-    </div>
-  );
+	return (
+		<div className='demo'>
+			<h2>eKYC dAPP</h2>
+			{!state.artifact ? (
+				<NoticeNoArtifact />
+			) : !state.contract ? (
+				<NoticeWrongNetwork />
+			) : (
+				demo
+			)}
+		</div>
+	);
 }
 
 export default Demo;
