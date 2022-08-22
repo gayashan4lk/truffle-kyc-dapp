eefa52309f9e70ee840594e7eb01e172dbb0254d
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-08-22 20:12:19 +0530

    add conditional rendering to profile page

--- client/src/components/navigation/mainNavBar.js
+++ client/src/components/navigation/mainNavBar.js
@@ -1,7 +1,18 @@
+// import { useState, useEffect } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+// import useEth from '../../contexts/EthContext/useEth';
 
 function MainNavBar() {
+	// const {
+	// 	state: { contract, accounts },
+	// } = useEth();
+	// const [address, setAddress] = useState('');
+
+	// useEffect(() => {
+	// 	setAddress(accounts[0]);
+	// }, []);
+
 	const appName = process.env.REACT_APP_NAME;
 	return (
 		<Navbar bg='light' expand='lg'>
@@ -20,6 +31,7 @@ function MainNavBar() {
 						</Link>
 					</Nav>
 				</Navbar.Collapse>
+				{/* <h6>Connected Account : {address}</h6> */}
 			</Container>
 		</Navbar>
 	);
