9b16f4a02f8ea80e036b0a0e70269837e293dd68
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-08-23 18:39:09 +0530

    holderview and issuerview created

---
+++ client/src/pages/profile/issuerView.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import useEth from '../../contexts/EthContext/useEth';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import LoadingCircle from '../../components/navigation/utilities/loadingCircle';
+
+const IssuerView = () => {
+	const {
+		state: { contract, accounts },
+	} = useEth();
+
+	const { register, handleSubmit } = useForm();
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [isSuccess, setIsSuccess] = useState(false);
+
+	const onSubmit = (data) => {
+		console.log(data);
+		setIsLoading(true);
+		createCredential(data);
+	};
+
+	const createCredential = async (data) => {
+		let result = await contract.methods
+			.createCredential(data.holder, data.definition)
+			.send({ from: accounts[0] });
+		console.log(result);
+		if (result) {
+			setIsLoading(false);
+			if (result.status) {
+				setIsSuccess(true);
+			} else {
+				setIsError(true);
+			}
+		}
+	};
+
+	let pageContent;
+	if (isLoading) {
+		pageContent = <LoadingCircle />;
+	} else if (isError) {
+		pageContent = <h1>Error Occured!</h1>;
+	} else if (isSuccess) {
+		pageContent = <h1>Credential issued successfully.</h1>;
+	} else {
+		pageContent = (
+			<div>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Form.Group className='mb-3' controlId='holderAddress'>
+						<Form.Label>Holder Address</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Enter Holder Address'
+							{...register('holder')}
+						/>
+					</Form.Group>
+					<Form.Group className='mb-3' controlId='certificateDetails'>
+						<Form.Label>Certificate Details</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Enter Cerificate'
+							{...register('definition')}
+						/>
+					</Form.Group>
+					<Button variant='primary' type='submit'>
+						Issue
+					</Button>
+				</form>
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<h3>Issue a certificate</h3>
+			{pageContent}
+		</div>
+	);
+};
+
+export default IssuerView;
