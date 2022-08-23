a74eea014d2d8c2ae99df691fbede5f784d08a49
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-08-24 01:55:58 +0530

    create basic verifier-view

--- client/src/pages/profile/verifierView.js
+++ client/src/pages/profile/verifierView.js
@@ -10,12 +10,36 @@ const VerfierView = () => {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 
-	const getSharedCredentials = async () => {};
+	const getAllSharedCredentials = async () => {
+		let result = await contract.methods
+			.getAllSharedCredentials()
+			.call({ from: accounts[0] });
+		return result;
+	};
+
+	useEffect(() => {
+		(async () => {
+			setIsLoading(false);
+			let result = await getAllSharedCredentials();
+			setData(result);
+			setIsLoading(false);
+		})();
+	}, []);
 
 	if (!isLoading) {
 		return (
 			<div>
-				<h1>shared cred list</h1>
+				<table>
+					<tbody>
+						{data.map((cred) => (
+							<tr key={cred.CredentialId}>
+								<td>{cred.CredentialId}</td>
+								<td>{cred.Definition}</td>
+								<td></td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 		);
 	} else {
